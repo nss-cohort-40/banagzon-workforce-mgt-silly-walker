ded7f1f7ddef2091c6064e769f175b1aaea9b3fc
Kirk Sudduth <kirksudduth@gmail.com>
2020-08-11 14:31:00 -0500

    got db connected and then got computer list showing with links to details pages

--- hrapp/urls.py
+++ hrapp/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', logout_user, name='logout'),
     path('employees/', employee_list, name='employee_list'),
+    path('computers/', computer_list, name='computer_list'),
+    path('computers/<int:computer_id>/', computer_details, name='computer')
+
 ]
---
+++ hrapp/views/computers/details.py
@@ -0,0 +1,34 @@
+import sqlite3
+from django.urls import reverse
+from django.shortcuts import render, redirect
+from ..connection import Connection
+
+
+def get_computer(computer_id):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.make,
+            c.purchase_date,
+            c.decommission_date
+        FROM hrapp_computer c
+        WHERE c.id = ?
+        """, (computer_id,))
+
+        return db_cursor.fetchone()
+
+
+def computer_details(request, computer_id):
+    if request.method == 'GET':
+        computer = get_computer(computer_id)
+
+        template = 'computers/detail.html'
+        context = {
+            'computer': computer
+        }
+
+        return render(request, template, context)
