6ab75bcc5f7be5a2a61c82d342c2f728065abd57
Tom Strother <tom_strother@yahoo.com>
2020-08-11 11:57:16 -0500

    initial group commit with group code

--- hrapp/models/employee.py
+++ hrapp/models/employee.py
@@ -1,13 +1,15 @@
 from django.db import models
-# from .department import Department
+from .department import Department
+
 
 class Employee(models.Model):
 
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     start_date = models.DateField()
+    department = models.ForeignKey(
+        Department, on_delete=models.CASCADE, default=1)
     is_supervisor = models.BooleanField()
-    # department = models.ForeignKey(Department, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = ("Employee")
