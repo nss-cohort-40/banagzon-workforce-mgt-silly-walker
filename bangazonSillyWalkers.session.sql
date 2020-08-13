778953df245c9a30dfcc2ba5a92f72bdccacf39b
Sarah Holder <sr.holder@yahoo.com>
2020-08-13 09:11:58 -0500

    wip

--- bangazonSillyWalkers.session.sql
+++ bangazonSillyWalkers.session.sql
@@ -1,12 +1,15 @@
 SELECT
     d.name,
-    d.budget, 
-    d.id department_id, 
+    d.budget,  
     e.first_name,
-    e.last_name
+    e.last_name,
+    e.department_id 
 FROM hrapp_department d
-JOIN hrapp_employee e on d.id = e.department_id
+JOIN hrapp_employee e on d.id = e.department_id;
 
+SELECT COUNT(department_id) as dept_count
+FROM hrapp_employee
+where department_id = 2;
 
 select
   p.id program_id,
--- hrapp/views/departments/department_details.py
+++ hrapp/views/departments/department_details.py
@@ -3,6 +3,19 @@ from django.urls import reverse
 from django.shortcuts import render, redirect
 from ..connection import Connection
 
+def get_count(department_id):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT COUNT(department_id) as dept_count
+        FROM hrapp_employee 
+        WHERE department_id = ?
+        """, (department_id))
+        
+
+
 def get_department(department_id):
     with sqlite3.connect(Connection.db_path) as conn:
         conn.row_factory = sqlite3.Row
@@ -12,12 +25,12 @@ def get_department(department_id):
         SELECT
             d.name,
             d.budget, 
-            d.id as department_id, 
+            d.id as dept_id, 
             e.first_name,
             e.last_name
         FROM hrapp_department d
-        JOIN hrapp_employee e on d.id = e.department_id
-        WHERE id = ?
+        JOIN hrapp_employee e on d.id = e.department_id 
+        WHERE d.id = ?
         """, (department_id,))
 
         return db_cursor.fetchone()
@@ -25,9 +38,10 @@ def get_department(department_id):
 def department_details(request, department_id):
     if request.method == 'GET':
         department = get_department(department_id)
+        
         template = 'departments/departments_detail.html'
         context = {
-            'department': department
+            'department': department,
         }
 
     return render(request, template, context)
