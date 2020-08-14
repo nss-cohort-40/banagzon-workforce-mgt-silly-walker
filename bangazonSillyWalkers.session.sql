bc1c27ceddc1ce8916fd0f4c645f686c065c334c
Tom Strother <tom_strother@yahoo.com>
2020-08-14 09:50:00 -0500

    updated sql file

--- bangazonSillyWalkers.session.sql
+++ bangazonSillyWalkers.session.sql
@@ -1,3 +1,6 @@
+
+-- SQL to create the join table for Employee Training Programs [start]
+;
 CREATE TABLE "hrapp_employee_training_program" (
 	"id" INTEGER NOT NULL PRIMARY KEY,
   "employee_id" INTEGER NOT NULL,
@@ -8,8 +11,11 @@ CREATE TABLE "hrapp_employee_training_program" (
 (`trainingprogram_id`) REFERENCES "hrapp_program"
 (`Id`)
 );
+-- [END of table creation]
+-- ;
 
-
+-- SQL to create some seed data for assinging training programs and employees
+;
 INSERT INTO hrapp_employee_training_program
   (employee_id, trainingprogram_id)
 VALUES
@@ -35,7 +41,6 @@ INSERT INTO hrapp_employee_training_program
 VALUES
   (2, 2);
 
-
 INSERT INTO hrapp_employee_training_program
   (employee_id, trainingprogram_id)
 VALUES
@@ -44,33 +49,7 @@ INSERT INTO hrapp_employee_training_program
   (employee_id, trainingprogram_id)
 VALUES
   (3, 1);
+-- [END of Seed Data creation]
+-- ;
 
 
-select
-  p.id program_id,
-  p.title,
-  p.end_date,
-  p.capacity,
-  p.start_date,
-  emp.id EmployeeId,
-  emp.first_name,
-  emp.last_name
-from hrapp_program p
-  LEFT join hrapp_employee_training_program tp on tp.trainingprogram_id = p.id
-  LEFT join hrapp_employee emp on tp.employee_id = emp.id
-WHERE p.id = 7
-WHERE p.start_date <= "2020-08-11";
-
-SELECT
-  c.id,
-  c.make,
-  c.model,
-  c.purchase_date,
-  c.decommission_date,
-  ec.employee_id emp_id
-FROM hrapp_computer c
-  LEFT JOIN hrapp_employeecomputer ec on c.id = ec.computer_id;
-
-SELECT
-  u.is_superuser
-FROM auth_user u;
